cc3b995936c8eb8c1a1662df8bc3f3c9a03cba32
Daniel Marchi <marchimail@gmail.com>
2020-05-11 10:18:51 -0300

    meu primeiro comit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace dev004_Carros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escolha seu carro:");
+            var carroEscolha = Console.ReadLine();
+
+            string mensagem;
+
+            if (carroEscolha == "brazilia")
+            {
+                mensagem = "Brazilia não carro, é carroça!";
+            }
+            else
+            {
+                mensagem = $"O carro escolhido foi: {carroEscolha}";
+            }
+
+            Console.WriteLine(mensagem);
+        }
+    }
+}
